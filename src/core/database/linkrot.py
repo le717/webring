9d766b3fa68109a9ffb7620f39f52fb65a534778
Caleb Ely <le717@users.noreply.github.com>
2021-08-11 16:34:02 -0400

    Impl more advanced linkrot checking
    Ref #2

--- src/core/database/linkrot.py
+++ src/core/database/linkrot.py
@@ -1,8 +1,9 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 import requests
 
-from src.core.database import weblink as db
+from src.core.database import weblink
+from src.core.database.schema import RottedLinks, WebLink, db
 from src.core.models.RotStates import RotStates
 
 
@@ -10,30 +11,87 @@ __all__ = ["check_all", "check_one"]
 
 
 class RotResult(TypedDict):
-    uuid: str
+    id: str
     url: str
     result: str
 
 
-def __check(url: str) -> RotStates:
+def __ping_url(url: str) -> bool:
     """Check a link for rotting."""
     try:
         r = requests.head(url)
-        if r.status_code == requests.codes.ok:
-            return RotStates.NO.value
+        return r.status_code == requests.codes.ok
     except Exception:
-        return RotStates.YES.value
+        return False
+
+
+def __create(data: WebLink) -> Literal[True]:
+    rot_entry = RottedLinks(id=data.id, times_failed=1)
+    db.session.add(rot_entry)
+    db.session.commit()
+    db.session.refresh(rot_entry)
+    return True
+
+
+def __get(uuid: str) -> RottedLinks:
+    return RottedLinks.query.filter_by(id=uuid).first()
+
+
+def __update(data: RottedLinks) -> Literal[True]:
+    db.session.query(RottedLinks).filter_by(id=data.id).update(
+        {"times_failed": data.times_failed + 1}, synchronize_session="fetch"
+    )
+    db.session.commit()
+    return True
+
+
+def __delete(uuid: str):
+    if exists := __get(uuid):
+        db.session.delete(exists)
+        db.session.commit()
+    return True
 
 
 def check_all() -> list[RotResult]:
     """Check all links for rotting."""
     results = []
-    for link in db.get_all():
-        results.append(RotResult(uuid=link.id, url=link.url, result=__check(link.url)))
+    for link in weblink.get_all():
+        results.append(RotResult(id=link.id, url=link.url, result=__ping_url(link.url)))
     return results
 
 
 def check_one(uuid: str) -> RotResult:
     """Check a single link for rotting."""
-    link = db.get(uuid)
-    return RotResult(uuid=link.id, url=link.url, result=__check(link.url))
+    # The site could be pinged, so we all good
+    link = weblink.get(uuid)
+    if __ping_url(link.url):
+        result = RotResult(id=link.id, url=link.url, result=RotStates.NO)
+        __delete(uuid)
+
+    # We could not ping the site, decide the next step
+    else:
+        result = RotResult(id=link.id, url=link.url, result=__record_failure(link))
+    return result
+
+
+def __record_failure(data: WebLink) -> RotStates:
+    TIMES_FAILED_THRESHOLD = 5
+
+    existing = __get(data.id)
+
+    # We don't have an existing failure record, so make one
+    if existing is None:
+        __create(data)
+        weblink.update({"id": data.id, "rotted": RotStates.MAYBE.value})
+        return RotStates.MAYBE
+
+    # We have an existing failure record, update the failure count
+    print(existing.times_failed)
+    if existing.times_failed < TIMES_FAILED_THRESHOLD:
+        __update(existing)
+        return RotStates.MAYBE
+
+    # The failure has occurred too often, declare a dead link
+    weblink.update({"id": data.id, "rotted": RotStates.YES.value})
+    __delete(data.id)
+    return RotStates.YES
