3702d0e3828d37d2197ce22f830ed274373adc64
Caleb Ely <le717@users.noreply.github.com>
2021-10-28 16:17:03 -0400

    Only run migrations when needed

--- src/app_factory.py
+++ src/app_factory.py
@@ -2,6 +2,8 @@ from os import fspath
 from importlib import import_module
 import logging
 
+from alembic import command
+from alembic.config import Config
 from flask import Flask
 from flask_cors import CORS
 from flask_smorest import Api
@@ -46,13 +48,17 @@ def create_app():
     # Create a database connection
     db_path = sys_vars.get_path("DB_PATH").resolve()
     with app.app_context():
-        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{fspath(db_path)}"
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path.as_posix()}"
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+        db.init_app(app)
 
-    # Create the database if needed
-    if not db_path.exists():
-        with app.app_context():
+        # Create the database if needed
+        if not db_path.exists():
             db.create_all()
+
+            # Tell Alembic this is a new database and we don't need
+            # to update it to a newer schema
+            alembic_cfg = Config("alembic.ini")
+            command.stamp(alembic_cfg, "head")
 
     return app
