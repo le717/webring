2acb66cff8991a976093b96962cf93fb2c7f5bd3
Caleb Ely <le717@users.noreply.github.com>
2024-08-16 12:42:00 -0400

    Make sure all meta keys are provided

--- src/core/database/meta.py
+++ src/core/database/meta.py
@@ -1,3 +1,5 @@
+from json import JSONDecodeError
+
 import sys_vars
 from flask import current_app
 
@@ -7,8 +9,19 @@ __all__ = ["get"]
 
 def get() -> dict[str, str]:
     """Get meta information about this webring instance."""
-    # TODO: Add defaults for webring.json
-    return sys_vars.get_json("webring.json") | {
+    # Attempt to fetch the webring metadata json
+    try:
+        meta = sys_vars.get_json("webring.json", default={})
+    except JSONDecodeError:
+        meta = {}
+
+    # If any required keys are missing, fill them back in
+    required_keys = ["name", "maintainer", "home_url"]
+    for k in required_keys:
+        if k not in meta:
+            meta[k] = ""
+
+    return meta | {
         "software": "https://github.com/le717/webring",
         "version": current_app.config["API_VERSION"],
     }
